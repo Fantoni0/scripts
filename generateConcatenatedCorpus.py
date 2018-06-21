cc9fef02c40d736c90782e6e50324bdfd18f175e
Fantoni0 <thisisnothate@gmail.com>
2018-06-21 09:55:52 +0200

    Concatenate and deconcatenate sentences

---
+++ generateConcatenatedCorpus.py
@@ -0,0 +1,45 @@
+#!/home/fantonio/miniconda2/bin/python
+# -*- coding: utf-8 -*-
+# It concatenates previous sentences and current sentences in one line: prev BREAK curr
+# fantoni[o|0] (19/6/18)
+
+import argparse
+import subprocess
+import sys
+import operator
+import random
+
+'''
+    It assumes the dataset files are in the form: training.l1, training.l2, dev.l1, dev.l2, test.l1, test.l2 
+'''
+parser = argparse.ArgumentParser(description="Takes a route to the dataset files and computes average length of sentences and words", formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('-ds', '--dataset', type=str, help="Dataset Locations")
+parser.add_argument('-n', '--sentences', type=int, help="Number of sentences to concatenate")
+parser.add_argument('-s', '--source', type=str, help="Source language of the splits")
+parser.add_argument('-t', '--target', type=str, help="Target languages of the split. Use the sema order as in datasets")
+
+# Get parameters
+args = parser.parse_args()
+dataset =  args.dataset
+sent = args.sentences
+source = args.source
+target = args.target
+
+# Create Folder
+folderName = 'concatenated-%d' %  sent
+createFolder = 'mkdir -p %s/%s' % (dataset, folderName)
+#subprocess.call(createFolder, shell=False)
+
+# Concatenate sentences
+for idx, s in enumerate(['training', 'dev', 'test']):
+    for l in [source, target]:                      
+        f = open(dataset+'/'+s+'.'+l, 'r')
+	fo = open(dataset+'/'+folderName+'/'+s+'.'+l, 'w')
+        fl = f.readlines()
+	fl = [line.strip() for line in fl] # Remove \n 
+	nfl = sent*['<pad>'] + fl
+        for i in range(len(fl)):
+	    fo.write(' BREAK '.join(nfl[i:i+sent])+' BREAK '+''.join(fl[i])+'\n')
+    f.close()
+    fo.close()
+      
--- lowerdetok.sh
+++ lowerdetok.sh
@@ -1,15 +1,15 @@
 # Script to lowercase and tokenize a corpus using moses scripts
 # Requires Moses
 # fantoni[o|0]
-if [$# -lt 2]; then
-	echo "Usage: `basename $0` file language"
-	echo "Example: lowerdetok.sh training.de de"
+if [ $# -lt 3 ]; then
+	echo "Usage: `basename $0` folder file language"
+	echo "Example: lowerdetok.sh data/ training.de de"
 	exit 1
 fi
 
 moses=/home/fantonio/software/mosesdecoder/scripts/tokenizer
 
-mkdir -p lowercased
-${moses}/lowercase.perl -l $2 < $1 > tmp.txt
-${moses}/tokenizer.perl -l $2 -no-escape < tmp.txt > lowercased/$1
+mkdir -p nmtTok
+${moses}/lowercase.perl -l $3 < $1/$2 > tmp.txt
+${moses}/tokenizer.perl -l $3 -no-escape < tmp.txt > nmtTok/$2
 rm tmp.txt
