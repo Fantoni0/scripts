1ee4f568e67532eeed7eb55d89b722a5583f2eab
Fantoni0 <thisisnothate@gmail.com>
2018-07-23 11:21:18 +0200

    Allow different deconcateancion indexes

--- undoConcatenation.py
+++ undoConcatenation.py
@@ -11,14 +11,14 @@ import sys
 '''
 parser = argparse.ArgumentParser(description="Takes a route to the dataset files and computes average length of sentences and words", formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('-f', '--path', type=str, help="File Locations")
-parser.add_argument('-p', '--prev_sentences', type=int, help="Number of previous senteces concatenated")
-parser.add_argument('-s', '--save', action='store_true', default=False, help="Optional parameter to save previous sentences in a separated file")
+parser.add_argument('-i', '--index', type=int, help="index of the sentence we want to deconcatenate (Starts at 0)")
+parser.add_argument('-s', '--save', action='store_true', default=False, help="Optional parameter to save previous sentences (with respect to --index) in a separated file")
 
 
 # Get parameters
 args = parser.parse_args()
 path =  args.path
-p = args.prev_sentences
+idx = args.index
 save = args.save
 
 # Get name and file extension
@@ -38,11 +38,11 @@ for i in range(len(fl)):
     split_s = fl[i].split(' BREAK ')
     special_case = i == 0 or i == len(fl) - 1
 
-    if i == 0 and save: # If it is the first line do nothing. 
+    if i == 0 and save: # If it is the first line do nothing. I think I should remove this line, why is this shit here ? 
         print()
     elif i == len(fl) - 1 and save: #If it is the last line we need to save both sentences
-        fp.write(split_s[p-1]+'\n')        
-        fp.write(split_s[p]+'\n')                
+        fp.write(split_s[idx-1]+'\n')
+        fp.write(split_s[idx]+'\n')
     
     if len(split_s) == 1:
         if save and not special_case:
@@ -50,8 +50,8 @@ for i in range(len(fl)):
         fo.write(fl[i]+'\n')
     else:
         if save and not special_case:
-            fp.write(split_s[p-1]+'\n')
-        fo.write(split_s[p]+'\n')
+            fp.write(split_s[idx-1]+'\n')
+        fo.write(split_s[idx]+'\n')
 f.close()
 fo.close()
 if save:
