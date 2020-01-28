1d82245694d10702488d6b6409abcc89931129ab
fantonio <antoniomlf@protonmail.com>
2020-01-28 12:17:58 +0100

    Remove duplicates

--- coco_sort.sh
+++
@@ -1,5 +0,0 @@
-# Simple script to sort values from nmt-keras (https://github.com/lvapeab/nmt-keras)
-# Sorts by the fifth column (Bleu_4) value
-# fantoni[o|0] (26/6/16)
-
-tail -n +2 $1 | sort -r -t ',' -k 5
